8253c5a768fcefef8f307806f6833984d8774c56
ElenaM-22 <122303552+ElenaM-22@users.noreply.github.com>
2025-09-17 09:28:54 -0400

    Update java.js

--- csce242-f25/assignments/05java-intro/java.js
+++ csce242-f25/assignments/05java-intro/java.js
@@ -19,9 +19,8 @@ document.getElementById("color-picker").addEventListener("input",(event) => {
 });
 
 document.getElementById("weather-image").onclick = (event) => {
-    if(document.getElementById("weather-image").source.includes("sun.jpg")){
+    if(document.getElementById("weather-image").src = "sun.jpg"{
         document.getElementById("weather-image").src = "https://static.vecteezy.com/system/resources/thumbnails/004/792/792/small/cloud-on-white-background-free-vector.jpg";
-
     }
     else{
         document.getElementById("weather-image").src = "sun.jpg";
