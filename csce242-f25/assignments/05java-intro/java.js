cddb9229f9f1763f7a96a9ecb644a4cb236738e6
ElenaM-22 <122303552+ElenaM-22@users.noreply.github.com>
2025-09-17 10:01:28 -0400

    Update java.js

--- csce242-f25/assignments/05java-intro/java.js
+++ csce242-f25/assignments/05java-intro/java.js
@@ -1,7 +1,6 @@
-document.getElementById("sunny-column").onclick = (event) => {
+document.getElementById("sunny-column").onclick = () => {
   document.getElementById("sunny-text").innerHTML = 
      "Here comes the sun Sun sun Sun here it comes";
-   event.currentTarget.classList.add("clicked");
 };
 
 document.getElementById("color-picker").addEventListener("input",(event) => {
